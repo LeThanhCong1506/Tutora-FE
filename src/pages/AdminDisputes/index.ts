d15a423b539af6a0a68e1a45d1f0b5d743fc105f
DuongThanhPhat-09 <duongthanhphat010104@gmail.com>
2026-06-10 16:53:34 +0700

    chore: remove admin module (moved to tutora-admin-frontend)

--- src/pages/AdminDisputes/index.ts
+++
@@ -1,1 +0,0 @@
-export { default as AdminDisputesPage } from './AdminDisputesPage';
--- src/pages/AdminSettings/components/index.ts
+++
@@ -1,2 +0,0 @@
-export { default as SubjectsManagement } from './SubjectsManagement';
-export { default as SubjectModal } from './SubjectModal';
