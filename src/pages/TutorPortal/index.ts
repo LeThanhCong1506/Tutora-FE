78450e07358d09b52f0c60e1eee7e2a0e649fccf
DuongThanhPhat-09 <duongthanhphat010104@gmail.com>
2026-07-19 12:37:40 +0700

    refactor(tutor-portal): migrate class detail flow to class sessions

--- src/pages/TutorPortal/index.ts
+++ src/pages/TutorPortal/index.ts
@@ -2,6 +2,6 @@ export { default as TutorPortalProfile } from './TutorPortalProfile';
 export { default as TutorPortalDashboard } from './TutorPortalDashboard';
 export { default as TutorPortalMessages } from './TutorPortalMessages';
 export { default as TutorPortalCalendar } from './TutorPortalCalendar';
-export { default as TutorPortalClassDetail } from './TutorPortalClassDetail';
+export { default as TutorPortalClassSessionDetail } from './TutorPortalClassSessionDetail';
 export { default as TutorPortalStudentProfile } from './TutorPortalStudentProfile';
 export { default as TutorPortalBookings } from './TutorPortalBookings';
